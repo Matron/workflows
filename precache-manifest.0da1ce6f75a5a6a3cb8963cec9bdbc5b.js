8494b8f159b17d40a2fbe775af2c6ddb35dcb986
matron <matias.rondan@gmail.com>
2020-10-28 18:27:23 -0300

    Updates

---
+++ precache-manifest.0da1ce6f75a5a6a3cb8963cec9bdbc5b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55cdd0222809e461df520a10f705988c",
+    "url": "/workflows/index.html"
+  },
+  {
+    "revision": "295fd8b111026cb07b18",
+    "url": "/workflows/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "de1c7f53593edcd85dac",
+    "url": "/workflows/static/js/2.dcf3cc00.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/workflows/static/js/2.dcf3cc00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "295fd8b111026cb07b18",
+    "url": "/workflows/static/js/main.4f52e490.chunk.js"
+  },
+  {
+    "revision": "a69ab777f58077fdf78e",
+    "url": "/workflows/static/js/runtime-main.0a206865.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/workflows/static/media/logo.5d5d9eef.svg"
+  }
+]);
